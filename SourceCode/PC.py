c5c02b38c7531ac7a7c10537fbae71b14fcfe542
shashank-simha <shashanksimha183@gmail.com>
2018-12-06 21:17:42 +0530

    PC codes updated

--- SourceCode/PC.py
+++ SourceCode/PC.py
@@ -1,7 +1,6 @@
 import serial
-from time import sleep, process_time
+from time import sleep, time
 from timeit import default_timer as timer
-
 
 def crc8(data):
 	crc = 0
@@ -24,19 +23,32 @@ print("Transmitting the message", end="")
 
 message = "Finance Minister Arun Jaitley Tuesday hit out at former RBI governor Raghuram Rajan for predicting that the next banking crisis would be triggered by MSME lending, saying postmortem is easier than taking action when it was required. Rajan, who had as the chief economist at IMF warned of impending financial crisis of 2008, in a note to a parliamentary committee warned against ambitious credit targets and loan waivers, saying that they could be the sources of next banking crisis. Government should focus on sources of the next crisis, not just the last one. In particular, government should refrain from setting ambitious credit targets or waiving loans. Credit targets are sometimes achieved by abandoning appropriate due diligence, creating the environment for future NPAs,\" Rajan said in the note.\"Both MUDRA loans as well as the Kisan Credit Card, while popular, have to be examined more closely for potential credit risk. Rajan, who was RBI governor for three years till September 2016, is currently"
 
-for i in range(20):
-	ser.write(message[i*60:i*60+60].encode())
-	print(".", end="")
-	sleep(0.5)
+time_init = time()
+for i in range(len(message)):
+	ser.write(message[i].encode())
+	sleep(0.005)
+time_final = time()
+
+transmit_rate = (len(message)*12) / ((time_final-time_init)) #including stopbits and stopbits
 
 ser.write("\n".encode())
 ser.write("exit\n".encode())
 sleep(1)
+
 print("\nReceiving the message")
+
+data_len=0
+time_init = time()
 for i in range(20):
 	data = ser.read(9999)
 	if len(data) > 0:
 		print(data.decode(), end="")
-	sleep(0.5)
+		data_len+=len(data)
+	sleep(0.25)
+time_final = time()
+
+receive_rate = (data_len*12) / ((time_final-time_init))
+
+print("\n\nTransmission rate: {0}\nReception rate: {1}\n".format(transmit_rate,receive_rate))
 
 ser.close()
